f53c68581e21d45aa8033b7da179c9d069466f5e
*Linda <hansonlindaime03@gmail.com>
2022-11-06 16:13:09 +0000

    update to package.json

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ const authRoutes = require("./route/authentication");
 
 
 // app constants 
-const { PORT,DATABASE } = require("./config");
+const { PORT, DATABASE } = require("./config");
 
 // Initialize the application
 const app = express()
 
 // templating engine to access html flies
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
 // middleware for static flies 
 app.use(express.static('public'))
@@ -35,7 +35,7 @@ app.use(cookieParser());
 
 //initializing middleware to parser body 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/signup', authRoutes);
 
@@ -43,29 +43,25 @@ app.use('/signup', authRoutes);
 
 
 // Getting routes
-app.get('/auth/register',(req,res)=>{
-  res.render('register')
+app.get('/auth/register', (req, res) => {
+    res.render('register')
 })
-app.get('/auth/login',(req,res)=>{
-  res.render('login')
+app.get('/auth/login', (req, res) => {
+    res.render('login')
 })
- app.get("/",homeController)
+app.get("/", homeController)
 
-app.get("/post/:id",checkUserData, getPostController)
+app.get("/post/:id", checkUserData, getPostController)
 
-app.get("/posts/new",checkUserData,PostController)
-  
-app.post("/posts/store",storePostController)
+app.get("/posts/new", checkUserData, PostController)
 
-  
+app.post("/posts/store", storePostController)
+
+
 
 // database connection
-mongoose.connect(DATABASE,{useNewUrlParser:true})
+mongoose.connect(DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
- //Listenting for the server on PORT
- app.listen(PORT, () => {console.log(`Server successfully started on http://localhost:${PORT}`)})
-
-
-
-
+//Listenting for the server on PORT
+app.listen(PORT, () => { console.log(`Server successfully started on http://localhost:${PORT}`) })
